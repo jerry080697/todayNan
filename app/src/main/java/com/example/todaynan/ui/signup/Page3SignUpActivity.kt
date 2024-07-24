dc09234b1ab8b53ee31b01d92d5db9784e978a19
jerry080697 <jerry0806@naver.com>
2024-07-24 21:52:01 +0900

    충돌 해결버전 커밋

--- app/src/main/java/com/example/todaynan/ui/signup/Page3SignUpActivity.kt
+++ app/src/main/java/com/example/todaynan/ui/signup/Page3SignUpActivity.kt
@@ -3,32 +3,17 @@ package com.example.todaynan.ui.signup
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-<<<<<<< HEAD
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.todaynan.data.entity.City
 import com.example.todaynan.data.entity.Location
 import com.example.todaynan.databinding.SignupPage3Binding
+import com.example.todaynan.ui.BaseActivity
 import com.example.todaynan.ui.adapter.CityRVAdapter
 import com.example.todaynan.ui.adapter.DistrictRVAdapter
 import com.example.todaynan.ui.adapter.DongRVAdapter
 
-class Page3SignUpActivity : AppCompatActivity() {
-
-    lateinit var binding: SignupPage3Binding
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = SignupPage3Binding.inflate(layoutInflater)
-        setContentView(binding.root)
-=======
-import com.example.todaynan.databinding.SignupPage1Binding
-import com.example.todaynan.databinding.SignupPage3Binding
-import com.example.todaynan.ui.BaseActivity
-
 class Page3SignUpActivity : BaseActivity<SignupPage3Binding>(SignupPage3Binding::inflate) {
     override fun initAfterBinding() {
->>>>>>> 7acdb94dba7e38592d38688a468c644ee21a2b10
 
         val allLocations = generateDummyItems()
         val cities = generateDummyItem()
@@ -53,6 +38,7 @@ class Page3SignUpActivity : BaseActivity<SignupPage3Binding>(SignupPage3Binding:
             finish()
         }
     }
+
     private fun onCityItemClick(city: City, allLocations: List<Location>) {
         // 클릭된 도시를 기준으로 Location 데이터 필터링
         val filteredDistricts = allLocations.filter { it.cityName == city.cityName }
@@ -69,7 +55,8 @@ class Page3SignUpActivity : BaseActivity<SignupPage3Binding>(SignupPage3Binding:
 
     private fun onDistrictItemClick(location: Location, allLocations: List<Location>) {
         // 클릭된 구역을 기준으로 Location 데이터 필터링
-        val filteredDongLocations = allLocations.filter { it.districtName == location.districtName }
+        val filteredDongLocations =
+            allLocations.filter { it.districtName == location.districtName }
 
         // DongRVAdapter 업데이트
         val dongAdapter = DongRVAdapter(filteredDongLocations)
@@ -79,65 +66,66 @@ class Page3SignUpActivity : BaseActivity<SignupPage3Binding>(SignupPage3Binding:
 
     private fun generateDummyItems(): List<Location> {
         val items = ArrayList<Location>()
-        items.add(Location("서울특별시","관악구","낙성대동"))
-        items.add(Location("서울특별시","관악구","난공동"))
-        items.add(Location("서울특별시","관악구","난항동"))
-        items.add(Location("서울특별시","관악구","남현동"))
-        items.add(Location("서울특별시","관악구","대학동"))
-        items.add(Location("서울특별시","관악구","미성동"))
-        items.add(Location("서울특별시","관악구","보라매동"))
-        items.add(Location("서울특별시","관악구","봉천동"))
-        items.add(Location("서울특별시","관악구","삼성동"))
-        items.add(Location("서울특별시","관악구","서림동"))
-        items.add(Location("서울특별시","관악구","서원동"))
-        items.add(Location("서울특별시","관악구","성현동"))
+        items.add(Location("서울특별시", "관악구", "낙성대동"))
+        items.add(Location("서울특별시", "관악구", "난공동"))
+        items.add(Location("서울특별시", "관악구", "난항동"))
+        items.add(Location("서울특별시", "관악구", "남현동"))
+        items.add(Location("서울특별시", "관악구", "대학동"))
+        items.add(Location("서울특별시", "관악구", "미성동"))
+        items.add(Location("서울특별시", "관악구", "보라매동"))
+        items.add(Location("서울특별시", "관악구", "봉천동"))
+        items.add(Location("서울특별시", "관악구", "삼성동"))
+        items.add(Location("서울특별시", "관악구", "서림동"))
+        items.add(Location("서울특별시", "관악구", "서원동"))
+        items.add(Location("서울특별시", "관악구", "성현동"))
 
-        items.add(Location("서울특별시","강남구","대치동"))
-
-
-        items.add(Location("경기도","광명시","철산동"))
-        items.add(Location("경기도","광명시","하안동"))
-        items.add(Location("경기도","광명시","소하동"))
-        items.add(Location("경기도","광명시","광명동"))
-        items.add(Location("경기도","광명시","노온사동"))
-        items.add(Location("경기도","광명시","학온동"))
-        items.add(Location("경기도","광명시","일직동"))
-
-        items.add(Location("경기도","안양시","비산동"))
+        items.add(Location("서울특별시", "강남구", "대치동"))
 
 
-        items.add(Location("인천광역시","미추홀구","주안동"))
+        items.add(Location("경기도", "광명시", "철산동"))
+        items.add(Location("경기도", "광명시", "하안동"))
+        items.add(Location("경기도", "광명시", "소하동"))
+        items.add(Location("경기도", "광명시", "광명동"))
+        items.add(Location("경기도", "광명시", "노온사동"))
+        items.add(Location("경기도", "광명시", "학온동"))
+        items.add(Location("경기도", "광명시", "일직동"))
 
-        items.add(Location("부산광역시","해운대구","반여동"))
+        items.add(Location("경기도", "안양시", "비산동"))
 
-        items.add(Location("울산광역시","중구","교동"))
 
-        items.add(Location("대구광역시","북구","검단동"))
+        items.add(Location("인천광역시", "미추홀구", "주안동"))
 
-        items.add(Location("광주광역시","서구","금호동"))
+        items.add(Location("부산광역시", "해운대구", "반여동"))
 
-        items.add(Location("대전광역시","동구","가양동"))
+        items.add(Location("울산광역시", "중구", "교동"))
 
-        items.add(Location("세종특별자치시","ㅁㅁ구","보람동"))
+        items.add(Location("대구광역시", "북구", "검단동"))
 
-        items.add(Location("강원도","원주시","우산동"))
+        items.add(Location("광주광역시", "서구", "금호동"))
 
-        items.add(Location("충청북도","괴산군","괴산읍"))
+        items.add(Location("대전광역시", "동구", "가양동"))
 
-        items.add(Location("충청남도","보령시","대청동"))
+        items.add(Location("세종특별자치시", "ㅁㅁ구", "보람동"))
 
-        items.add(Location("전라남도","여수시","동문동"))
+        items.add(Location("강원도", "원주시", "우산동"))
 
-        items.add(Location("전라북도","익산시","동산동"))
+        items.add(Location("충청북도", "괴산군", "괴산읍"))
 
-        items.add(Location("경상남도","김해시","구산동"))
+        items.add(Location("충청남도", "보령시", "대청동"))
 
-        items.add(Location("경상북도","안동시","광석동"))
+        items.add(Location("전라남도", "여수시", "동문동"))
 
-        items.add(Location("제주특별자치도","제주시","화북동"))
+        items.add(Location("전라북도", "익산시", "동산동"))
+
+        items.add(Location("경상남도", "김해시", "구산동"))
+
+        items.add(Location("경상북도", "안동시", "광석동"))
+
+        items.add(Location("제주특별자치도", "제주시", "화북동"))
 
         return items
     }
+
     private fun generateDummyItem(): List<City> {
         val item = ArrayList<City>()
         item.add(City("서울특별시"))
