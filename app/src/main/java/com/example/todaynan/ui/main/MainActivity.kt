0c3299cedf78bfdd8eab6ca352fc1d60a40f936e
thwldus <gee906@naver.com>
2024-07-28 14:10:49 +0900

    [MAIN] 바텀내비게이션뷰 선택 시 아이콘 변경

--- app/src/main/java/com/example/todaynan/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/todaynan/ui/main/MainActivity.kt
@@ -15,14 +15,23 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
     }
 
     private fun initBottomNavigation(){
+        binding.mainBnv.menu.findItem(R.id.searchFragment).setIcon(R.drawable.ic_search_on)
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, SearchFragment())
             .commitAllowingStateLoss()
 
         binding.mainBnv.setOnItemSelectedListener{ item ->
+            // 모든 아이템의 아이콘을 기본 아이콘으로 재설정
+            binding.mainBnv.menu.findItem(R.id.searchFragment).setIcon(R.drawable.ic_search_off)
+            binding.mainBnv.menu.findItem(R.id.locationFragment).setIcon(R.drawable.ic_location_off)
+            binding.mainBnv.menu.findItem(R.id.boardFragment).setIcon(R.drawable.ic_board_off)
+            binding.mainBnv.menu.findItem(R.id.myPageFragment).setIcon(R.drawable.ic_mypage_off)
+
+            // 선택된 항목의 아이콘 변경 및 Fragment 전환
             when (item.itemId) {
 
                 R.id.searchFragment -> {
+                    item.setIcon(R.drawable.ic_search_on)
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_frm, SearchFragment())
                         .commitAllowingStateLoss()
@@ -30,18 +39,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
                 }
 
                 R.id.locationFragment -> {
+                    item.setIcon(R.drawable.ic_location_on)
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_frm, LocationFragment())
                         .commitAllowingStateLoss()
                     return@setOnItemSelectedListener true
                 }
                 R.id.boardFragment -> {
+                    item.setIcon(R.drawable.ic_board_on)
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_frm, BoardFragment())
                         .commitAllowingStateLoss()
                     return@setOnItemSelectedListener true
                 }
                 R.id.myPageFragment -> {
+                    item.setIcon(R.drawable.ic_mypage_on)
                     supportFragmentManager.beginTransaction()
                         .replace(R.id.main_frm, MyPageFragment())
                         .commitAllowingStateLoss()
