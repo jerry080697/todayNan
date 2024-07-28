5fb1ad481aa3297d26b6500d5f80f6af51b169f8
thwldus <gee906@naver.com>
2024-07-28 13:03:12 +0900

    [SIGNUP] 지역정보 선택 시 아이템 색상 변경

--- app/src/main/java/com/example/todaynan/ui/adapter/LocationRVAdapter.kt
+++ app/src/main/java/com/example/todaynan/ui/adapter/LocationRVAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.todaynan.R
 import com.example.todaynan.data.entity.City
 import com.example.todaynan.data.entity.Location
 import com.example.todaynan.databinding.CityItemBinding
@@ -15,6 +16,8 @@ class CityRVAdapter(
     private val onItemClick: (City) -> Unit
 ) : RecyclerView.Adapter<CityRVAdapter.ViewHolder>() {
 
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding = CityItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(binding)
@@ -23,7 +26,21 @@ class CityRVAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.cityName.text = item.cityName
-        holder.itemView.setOnClickListener { onItemClick(item) }
+
+        // 아이템 클릭 시 동작 설정
+        holder.itemView.setOnClickListener {
+            val previousPosition = selectedPosition
+            selectedPosition = holder.bindingAdapterPosition
+            onItemClick(item)
+            notifyItemChanged(previousPosition)
+            notifyItemChanged(selectedPosition)
+        }
+        // 클릭된 아이템의 배경색 변경
+        if (position == selectedPosition) {
+            holder.itemView.setBackgroundResource(R.drawable.item_border_no_radius_selected)
+        } else {
+            holder.itemView.setBackgroundResource(R.drawable.item_border_no_radius)
+        }
     }
 
     override fun getItemCount(): Int = items.size
@@ -37,6 +54,8 @@ class DistrictRVAdapter(
     private val onItemClick: (Location) -> Unit
 ) : RecyclerView.Adapter<DistrictRVAdapter.ViewHolder>() {
 
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding = DistrictItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(binding)
@@ -45,7 +64,21 @@ class DistrictRVAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.districtName.text = item.districtName
-        holder.itemView.setOnClickListener { onItemClick(item) }
+
+        // 아이템 클릭 시 동작 설정
+        holder.itemView.setOnClickListener {
+            val previousPosition = selectedPosition
+            selectedPosition = holder.bindingAdapterPosition
+            onItemClick(item)
+            notifyItemChanged(previousPosition)
+            notifyItemChanged(selectedPosition)
+        }
+        // 클릭된 아이템의 배경색 변경
+        if (position == selectedPosition) {
+            holder.itemView.setBackgroundResource(R.drawable.item_border_no_radius_selected)
+        } else {
+            holder.itemView.setBackgroundResource(R.drawable.item_border_no_radius)
+        }
     }
 
     override fun getItemCount(): Int = items.size
@@ -59,6 +92,8 @@ class DongRVAdapter(
     private val onItemClick: (Location) -> Unit
 ) : RecyclerView.Adapter<DongRVAdapter.ViewHolder>() {
 
+    private var selectedPosition: Int = RecyclerView.NO_POSITION
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val binding = DongItemBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
         return ViewHolder(binding)
@@ -67,7 +102,21 @@ class DongRVAdapter(
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = items[position]
         holder.dongName.text = item.dongName
-        holder.itemView.setOnClickListener { onItemClick(item) } // Set the click listener
+
+        // 아이템 클릭 시 동작 설정
+        holder.itemView.setOnClickListener {
+            val previousPosition = selectedPosition
+            selectedPosition = holder.bindingAdapterPosition
+            onItemClick(item)
+            notifyItemChanged(previousPosition)
+            notifyItemChanged(selectedPosition)
+        }
+        // 클릭된 아이템의 배경색 변경
+        if (position == selectedPosition) {
+            holder.itemView.setBackgroundResource(R.drawable.item_border_no_radius_selected)
+        } else {
+            holder.itemView.setBackgroundResource(R.drawable.item_border_no_radius)
+        }
     }
 
     override fun getItemCount(): Int = items.size
