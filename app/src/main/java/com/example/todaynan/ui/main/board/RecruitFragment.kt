555ad91f46f4bf405258d1ac177aca5acf8827e3
thwldus <gee906@naver.com>
2024-08-04 00:51:36 +0900

    [BOARD] 게시판 초기화면

---
+++ app/src/main/java/com/example/todaynan/ui/main/board/RecruitFragment.kt
@@ -0,0 +1,10 @@
+package com.example.todaynan.ui.main.board
+
+import com.example.todaynan.databinding.FragmentRecruitBinding
+import com.example.todaynan.ui.BaseFragment
+
+class RecruitFragment: BaseFragment<FragmentRecruitBinding>(FragmentRecruitBinding::inflate) {
+    override fun initAfterBinding() {
+
+    }
+}
