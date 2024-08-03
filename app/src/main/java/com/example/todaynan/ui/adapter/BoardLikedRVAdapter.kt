58470554b22ba5cf92f5921bc70ffe51e212a4bf
thwldus <gee906@naver.com>
2024-08-03 17:43:33 +0900

    [MYPAGE] 게시물 아이템 크기 조정

--- app/src/main/java/com/example/todaynan/ui/adapter/BoardLikedRVAdapter.kt
+++ app/src/main/java/com/example/todaynan/ui/adapter/BoardLikedRVAdapter.kt
@@ -6,13 +6,13 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todaynan.R
-import com.example.todaynan.data.entity.MyLikedPost
-import com.example.todaynan.databinding.MyLikedPostBinding
+import com.example.todaynan.data.entity.Post
+import com.example.todaynan.databinding.ItemPostBinding
 
-class BoardLikedRVAdapter(private val items: List<MyLikedPost>) : RecyclerView.Adapter<BoardLikedRVAdapter.ViewHolder>() {
+class BoardLikedRVAdapter(private val items: List<Post>) : RecyclerView.Adapter<BoardLikedRVAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
-        val binding:MyLikedPostBinding= MyLikedPostBinding.inflate(LayoutInflater.from(viewGroup.context),viewGroup,false)
+        val binding: ItemPostBinding = ItemPostBinding.inflate(LayoutInflater.from(viewGroup.context),viewGroup,false)
         return ViewHolder(binding)
     }
 
@@ -22,7 +22,7 @@ class BoardLikedRVAdapter(private val items: List<MyLikedPost>) : RecyclerView.A
         holder.userName.text = item.userName
         holder.profileImg.setImageResource(R.drawable.default_profile_img)
         holder.userLocation.text = item.location
-        holder.boardTitle.text=item.boardTitle
+        //holder.boardTitle.text=item.boardTitle
         holder.creationTime.text= item.creationTime
         holder.likedNum.text= item.likedNum.toString()
         holder.replyNum.text=item.replyNum.toString()
@@ -34,14 +34,14 @@ class BoardLikedRVAdapter(private val items: List<MyLikedPost>) : RecyclerView.A
         return items.size
     }
 
-    inner class ViewHolder(val binding: MyLikedPostBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: ItemPostBinding) : RecyclerView.ViewHolder(binding.root) {
         val profileImg: ImageView = binding.likedPostProfileIv
         val userName: TextView = binding.likedPostUserNameTv
         val userLocation:TextView=binding.likedPostUserLocationTv
         val likedNum:TextView=binding.postLikeNumberTv
         val replyNum:TextView=binding.postReplyNumberTv
         val creationTime:TextView=binding.likedPostCreationTimeTv
-        val boardTitle:TextView=binding.likedPostBoardNameTv
+        //val boardTitle:TextView=binding.likedPostBoardNameTv
         val postTitle:TextView=binding.likedPostTitleTv
         val postContent:TextView=binding.likedPostContentTv
     }
